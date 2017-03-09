f64c58774310950ea23664e7ce856ccc5e6889eb
wellyogui <well_yogui@hotmail.com>
2017-03-09 02:01:34 -0300

    cardview em todo os tab do details

--- app/src/main/java/com/example/wellington/lolguide/view/ui/details/fragment/TipsFragment.java
+++ app/src/main/java/com/example/wellington/lolguide/view/ui/details/fragment/TipsFragment.java
@@ -37,7 +37,6 @@ public class TipsFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
